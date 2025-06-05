a16ec75ba8db25f9f81df9559c6549dae73d0a03
mangalittt <komangalitt1010@gmail.com>
2025-06-05 16:16:56 +0700

    pertemuan 14

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tutorial Laravel - www.malasngoding.com</title>
+</head>
+<body>
+
+	<h3>www.malasngoding.com</h3>
+	<p>Seri Tutorial Laravel Lengkap Dari Dasar</p>
+	<p>Ini adalah view blog. ada di route blog.</p>
+
+    <br>
+    <a href='satu'>pertama</a><br>
+    <br>
+    <a href='layout'>tugaslayout</a><br>
+    <br>
+    <a href='tugascss'>tugascss</a><br>
+    <br>
+    <a href='template1'>template1</a><br>
+    <br>
+    <a href='js1'>js1</a><br>
+    <br>
+    <a href='bootstrap1'>bootstrap1</a><br>
+    <br>
+    <a href='linktree'>linktree</a><br>
+    <br>
+    <a href='contactform'>contactform</a><br>
+    <br>
+    <a href='validasi1'>validasi1</a><br>
+    <br>
+    <a href='ets'>ets</a><br>
+
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,103 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PegawaiDBController;
+//import java.io;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+
+//System.out.println("hello world");
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('halo', function () {
+	return "<h1>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h1>";
+});
+
+Route::get('blog', function () {
+	return view('blog');
+});
+
+Route::get('satu', function () {
+	return view('pertama');
+});
+
+Route::get('layout', function () {
+	return view('tugaslayout');
+});
+
+Route::get('tugascss', function () {
+	return view('tugascss');
+});
+
+Route::get('template1', function () {
+	return view('template1');
+});
+
+Route::get('js1', function () {
+	return view('js1');
+});
+
+Route::get('bootstrap1', function () {
+	return view('bootstrap1');
+});
+
+Route::get('linktree', function () {
+	return view('linktree');
+});
+
+Route::get('contactform', function () {
+	return view('contactform');
+});
+
+Route::get('validasi1', function () {
+	return view('validasi1');
+});
+
+Route::get('ets', function () {
+	return view('index');
+});
+
+Route::get('frontend', function () {
+	return view('frontend');
+});
+
+Route::get('dosen', [DosenController::class, 'index']);
+
+Route::get('welcome', [DosenController::class, 'welcome']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+//route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
+//route pegawaiDB
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+Route::get('/pegawai/tambah', [PegawaiDBController::class, 'tambah']);
+Route::post('/pegawai/store', [PegawaiDBController::class, 'store']); //jika form dikirim, route ini akan dijalankan
+Route::get('/pegawai/edit/{id}',[PegawaiDBController::class, 'edit']);
+Route::post('/pegawai/update',[PegawaiDBController::class, 'update']);
+Route::get('/pegawai/hapus/{id}', [PegawaiDBController::class, 'hapus']);
+
+Route::get('/pegawai/cari', [PegawaiDBController::class, 'cari']);
